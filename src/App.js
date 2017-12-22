6a67fcce257b4516b3c151474eb71ec656dc4551
Paulo Roberto <paulo83psy@gmail.com>
2017-12-22 12:30:40 -0200

    finshed

--- src/App.js
+++ src/App.js
@@ -1,35 +1,284 @@
-import React, { Component } from 'react';
+import React,{Component} from 'react'
 import './App.css'
-import Books from './Books'
-import Home from './Home'
-import { Route } from 'react-router-dom'
+import SweetAlert from 'sweetalert-react';
+import BookCard from './BookCard'
+import If from "./If";
+import * as BooksAPI  from './BooksAPI';
 
 class BooksApp extends Component {
+  state = {
+    /**
+     * TODO: Instead of using this state variable to keep track of which page
+     * we're on, use the URL in the browser's address bar. This will ensure that
+     * users can use the browser's back and forward buttons to navigate between
+     * pages, as well as provide a good URL they can bookmark and share.
+     */
+    showSearchPage: false,
+    books:[],
+    myBooks:[],
+    showAdded:false,
+    showError:false,
+    booksRead:[],
+    booksCurrentlyReading:[],
+    booksWantToRead:[],
+  }
 
-    state = {
-        myBooks: []
+    updateQuery = (query) => {
+        if(query.length >= 3){
+            BooksAPI.search(query.trim()).then((books)=>{
+                if(books instanceof  Array){
+                  this.setState({ books: books })
+                    return
+                }
+            });
+        }
     }
-    removeBook = (book) => {
-        this.setState( (state) => ({
-            myBooks: state.myBooks.filter((_) => _.id !== book.id)
-        }))
+
+    componentWillMount(){
+        let myBooks = window.localStorage.getItem('myBooks') || '[]';
+        this.setState({ myBooks: JSON.parse(myBooks)});
+    }
+
+    updateLocalStorage =(books) =>{
+        window.localStorage.setItem('myBooks', JSON.stringify(books));
+        this.setState({showAddBook:true});
+    }
+
+    saveNewBook = (shelf, book) =>{
+        if( book !== undefined && book.id !== undefined && book.id !== Object){
+            this.setState(function (prev) {
+                let { myBooks =[]} = prev;
+                let oldBook = myBooks.filter((_)=> book.id == _.id);
+                if(oldBook.length == 0){
+                    const newBook = {
+                        id: book.id,
+                        title: book.title,
+                        description: book.description,
+                        imageLink: book.imageLink,
+                        previewLink: book.previewLink,
+                        authors:book.authors,
+                        shelf:shelf
+                    };
+
+                    myBooks.push(newBook);
+                    this.updateLocalStorage(myBooks);
+                }else{
+                    if(shelf !== 'wantToRead' && shelf !== book.shelf){
+                        this.updateShelf(book,shelf);
+                    }else{
+                        this.setState({showAdded:true});
+                    }
+                }
+                return {myBooks};
+            })
+        }else{
+            this.setState({showError:true});
+        }
 
     }
 
-  render() {
+    updateShelf(book, shelf){
+        let books  = this.state.myBooks;
+        let indexToRemove;
+        books.find(function(value, index){
+            if(value.id == book.id){
+                indexToRemove= index;
+                return;
+            }
+        });
+
+        books.splice(indexToRemove,1);
+        if(shelf !== 'none'){
+            book.shelf = shelf;
+            books.push(book);
+            BooksAPI.update(book, shelf);
+        }
+        window.localStorage.setItem('myBooks', JSON.stringify(books));
+    }
+
+    render() {
+        const book ={};
     return (
-        <div className="page-wrapper">
-            <Route exact path='/' render={() => (
-                <Home onDeleteBook={this.removeBook} contacts={this.state.myBooks}/>
-            )}/>
 
-            <Route exact path='/books' render={() => (
-                <Books/>
-            )}/>
+      <div className="app">
+        {this.state.showSearchPage ? (
+          <div className="search-books">
+            <div className="search-books-bar">
+              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+              <div className="search-books-input-wrapper">
+                {/*
+                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
+                  You can find these search terms here:
+                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
 
-        </div>
-    );
+                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+                  you don't find a specific author or title. Every search is limited by search terms.
+                */}
+                <input type="text" placeholder="Search by title or author" onChange={(event) => this.updateQuery(event.target.value)} />
+
+              </div>
+            </div>
+            <div className="search-books-results">
+              <div className="list-books">
+
+                    <ol className="books-grid">
+                    {this.state.books.map((book, index) => (
+                        <div key={book.id}>
+                            <BookCard
+                                onCreateSaveBook={(shelf, book)=> {
+                                    this.saveNewBook(shelf, book)
+                                }}
+                                id={book.id }
+                                title={book.title !== undefined ? book.title:"No description"}
+                                description={book.description !== undefined ? book.description : "No description"}
+                                imageLink={book.imageLinks !== undefined ? book.imageLinks.smallThumbnail :""}
+                                previewLink={book.previewLink !== undefined ? book.previewLink:""}
+                                authors={book.authors instanceof Array ? book.authors:[]}
+                            />
+                        </div>
+
+                     ))}
+                    </ol>
+
+            </div>
+            </div>
+          </div>
+        ) : (
+          <div className="list-books">
+            <div className="list-books-title">
+              <h1>MyReads</h1>
+            </div>
+            <div className="list-books-content">
+              <div>
+                <div className="bookshelf">
+                  <h2 className="bookshelf-title">Currently Reading</h2>
+                  <div className="bookshelf-books">
+                    <ol className="books-grid">
+
+                        <If test={this.state.myBooks.length == 0}>
+                            <div className="label">You don't have any book to reads yet.</div>
+                        </If>
+                        {this.state.myBooks.map((book, index) => (
+                            <If test={book.shelf === 'currentlyReading'} key={book.id}>
+
+                            <BookCard
+
+                                onCreateSaveBook={(shelf, book)=> {
+                                    this.saveNewBook(shelf, book)
+                                }}
+                                id={book.id }
+                                title={book.title}
+                                description={book.description !== undefined ? book.description:""}
+                                imageLink={book.imageLink}
+                                previewLink={book.previewLink !== undefined ? book.previewLink:""}
+                                authors={book.authors instanceof Array ? book.authors:[]}
+                                shelf={book.shelf}
+                            />
+
+                            </If>
+                        ))}
+                    </ol>
+                  </div>
+                </div>
+                <div className="bookshelf">
+                  <h2 className="bookshelf-title">Want to Read</h2>
+                  <div className="bookshelf-books">
+                    <ol className="books-grid">
+                        <If test={this.state.myBooks.length == 0}>
+                            <div className="label">You don't have any book to reads yet.</div>
+                        </If>
+                        {this.state.myBooks.map((book, index) => (
+                            <If test={book.shelf === 'wantToRead'} key={book.id}>
+                            <BookCard
+                                onCreateSaveBook={(shelf, book)=> {
+                                    this.saveNewBook(shelf, book)
+                                }}
+                                id={book.id }
+                                title={book.title}
+                                description={book.description !== undefined ? book.description:""}
+                                imageLink={book.imageLink}
+                                previewLink={book.previewLink !== undefined ? book.previewLink:""}
+                                authors={book.authors instanceof Array ? book.authors:[]}
+                                shelf={book.shelf}
+                            />
+                            </If>
+                        ))}
+
+                    </ol>
+                  </div>
+                </div>
+
+                <div className="bookshelf">
+                  <h2 className="bookshelf-title">Read</h2>
+                  <div className="bookshelf-books">
+                    <ol className="books-grid">
+                        <If test={this.state.myBooks.length == 0}>
+                            <div className="label">You don't have any book to read yet.</div>
+                        </If>
+                        {this.state.myBooks.map((book, index) => (
+                            <If test={book.shelf === 'read'} key={book.id}>
+                            <BookCard
+                                onCreateSaveBook={(shelf, book)=> {
+                                    this.saveNewBook(shelf, book)
+                                }}
+                                id={book.id }
+                                title={book.title}
+                                description={book.description !== undefined ? book.description:""}
+                                imageLink={book.imageLink}
+                                previewLink={book.previewLink !== undefined ? book.previewLink:""}
+                                authors={book.authors instanceof Array ? book.authors:[]}
+                                shelf={book.shelf}
+                            />
+                            </If>
+                        ))}
+
+                    </ol>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="open-search">
+              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+            </div>
+          </div>
+        )}
+
+        <SweetAlert
+            showCancelButton={true}
+            showConfirmButton={true}
+            show={this.state.showConfirmRemove}
+            title="Remove"
+            confirmButtonText={"Yes"}
+            cancelButtonText={"No"}
+            onClose={() => this.setState({ showConfirmRemove: false })}
+            text={'Would like to remove ' + book.title}
+            onCancel={() => this.setState({ showConfirmRemove: false })}
+            onConfirm={(e) => this.confirmDeleteBook(book)}
+        />
+
+        <SweetAlert
+            show={this.state.showAddBook}
+            title={"The book " + book.title + " was add."}
+            confirmButtonColor="green"
+            onConfirm={() => this.setState({ showAddBook: false })}
+        />
+
+        <SweetAlert
+            show={this.state.showAdded}
+            title={"The book already was added" }
+            confirmButtonColor="red"
+            onConfirm={() => this.setState({ showAdded: false })}
+        />
+
+        <SweetAlert
+            show={this.state.showError}
+            title={"Oops! Something wrong happened!!" }
+            confirmButtonColor="red"
+            onConfirm={() => this.setState({ showError: false })}
+        />
+      </div>
+    )
   }
 }
 
-export default BooksApp;
+export default BooksApp
--- src/BookCard.js
+++ src/BookCard.js
@@ -1,228 +1,79 @@
 import React, {Component} from 'react'
+import serializeForm from 'form-serialize'
 import PropTypes from 'prop-types'
-import If from "./If";
-import SweetAlert from 'sweetalert-react';
-import * as BooksAPI  from './BooksAPI';
+
+
 class BookCard extends Component{
 
     constructor(props) {
         super(props);
-            this.state = { myBooks: [],
-            verifyRemove:false,
-            verifyRead: false,
-            verifyWantToRead: false,
-            verifyCurrentlyReading:false,
-            showConfirmRemove: false,
-            showAddBook: false,
-            showAdded: false,
-            showError: false};
+        this.handleChange = this.handleChange.bind(this);
     }
+  static propTypes = {
+      id:PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+      imageLink: PropTypes.string.isRequired,
+      previewLink: PropTypes.string.isRequired,
+      shelf: PropTypes.string,
+      authors:PropTypes.array
+  }
 
-    static propTypes = {
-        id:PropTypes.string.isRequired,
-        title: PropTypes.string.isRequired,
-        description: PropTypes.string.isRequired,
-        imageLink: PropTypes.string.isRequired,
-        previewLink: PropTypes.string.isRequired,
-        verifyWantToRead: PropTypes.bool.isRequired,
-        updateLists: PropTypes.func.isRequired
+  handleSubmit = (shelf,book) => {
+    if (this.props.onCreateSaveBook){
+        this.props.onCreateSaveBook(shelf, book)
     }
+  }
 
-    updateLists(){
-        const myBooks = window.localStorage.getItem('myBooks') || '[]';
-        if (this.props.updateLists)
-            this.props.updateLists(JSON.parse( myBooks ))
-    }
-    componentWillMount(){
-        const myBooks = window.localStorage.getItem('myBooks') || '[]';
-        this.setState({ myBooks: JSON.parse( myBooks )});
-    }
-
-    updateLocalStorage =(books) =>{
-        window.localStorage.setItem('myBooks', JSON.stringify(books));
-        this.setState({ myBooks: JSON.stringify(books), showAddBook:true});
-    }
-
-    reading = (book)=>{
-        this.updateShelf(book, "currentlyReading")
-    }
-
-    read = (book)=> {
-        this.updateShelf(book, "read")
-    }
-
-    onDeleteBook = (book)=>{
-        this.setState({showConfirmRemove : true});
-    }
-
-    confirmDeleteBook = (book)=>{
-        let books  = this.state.myBooks;
-        let indexToRemove;
-
-        books.find(function(value, index){
-            if(value.id == book.id){
-                indexToRemove= index;
-                return;
-            }
-        });
-
-        books.splice(indexToRemove,1);
-        if(books.length == 0){
-            window.localStorage.clear();
-            this.setState({ myBooks: '[]'});
-        }else{
-            window.localStorage.setItem('myBooks', JSON.stringify(books));
-            this.setState({ myBooks: JSON.stringify( books )});
+    handleChange= (book, e) => {
+        e.preventDefault()
+        let shelf = e.target.value
+        if (this.props.onCreateSaveBook){
+            this.props.onCreateSaveBook(shelf, book)
         }
-        this.setState({showConfirmRemove : false});
     }
-
-    updateShelf(book, shelf){
-        let books  = this.state.myBooks;
-        let indexToRemove;
-        books.find(function(value, index){
-            if(value.id == book.id){
-                indexToRemove= index;
-                return;
-            }
-        });
-
-        books.splice(indexToRemove,1);
-        book.shelf = shelf;
-        books.push(book);
-
-        window.localStorage.setItem('myBooks', JSON.stringify(books));
-        this.setState({ myBooks: JSON.stringify( books )});
-
-        BooksAPI.update(book, book.shelf)
-
-    }
-    wantToRead= (book) =>{
-        if( book !== undefined && book.id !== undefined && book.id !== Object){
-            this.setState(function (prev) {
-                let { myBooks =[]} = prev;
-                let oldBook = myBooks.filter((_)=> book.id == _.id);
-                if(oldBook.length == 0){
-                    const newBook = {
-                        id: book.id,
-                        title: book.title,
-                        description: book.description,
-                        imageLink: book.imageLink,
-                        previewLink: book.previewLink,
-                        shelf:'wantToRead'
-                    };
-
-                    myBooks.push(newBook);
-                    this.updateLocalStorage(myBooks);
-
-                }else{
-                    this.setState({showAdded:true});
-                }
-                return {myBooks};
-            })
-        }else{
-            this.setState({showError:true});
-        }
-        browserHistory.push('/')
-    }
-
 
     render(){
-        const { id, title, description, imageLink, previewLink, verifyWantToRead, showError, verifyRemove, verifyRead, verifyCurrentlyReading,  } = this.props
-        const book ={id:id, title:title, description:description, imageLink:imageLink, previewLink:previewLink};
-        return(
-            <div className="col-12">
-                <div className="card" >
-                    <span className="card-header text-span" >{title}</span>
-                    <hr/>
-                    <div className="content">
+      const { id, title, description, imageLink, previewLink, authors, shelf} = this.props
+      const book ={id:id, title:title, description:description, imageLink:imageLink, previewLink:previewLink, authors:authors, shelf:shelf};
+    return(
+      <div>
+          <form>
+              <li key={id} name='avatarURL' className="card">
+                <div className="book">
+                  <div className="book-top">
+                    <div className="book-cover"  id='imageLink'
+                         style={{ width: 128, height: 193,
+                           backgroundImage: 'url('+imageLink+')' }} />
+                      <div className="book-shelf-changer" >
+                          <select id="shelf" onChange={(e) =>this.handleChange(book, e)}>
+                              <option value="none" disabled selected >Move to...</option>
+                              <option value="currentlyReading">Currently Reading</option>
+                              <option value="wantToRead" >Want to Read</option>
+                              <option value="read">Read</option>
+                              <option value="none">None</option>
+                          </select>
+                      </div>
 
-                        <div className="row">
-                            <div className="col-12">
-                                <div className="icon-big icon-warning text-center">
-                                    <img src={imageLink} id="imageLink"/>
-                                </div>
-                            </div>
-                            <div className="col-12">
-                                <div className="description">
-                                    <span className="text-span" id="description">{description}</span>
-                                </div>
-                            </div>
-                        </div>
+                  </div>
 
-                        <div className="footer">
-                            <hr />
-                            <div className="stats">
-                                <a className="btn orange" id="previewLink" title="Detail" href={previewLink} target="_blank">
-                                    <i className="fa fa-info" aria-hidden="true"></i>
-                                </a>
-
-                                <If test={verifyWantToRead == true}>
-                                    <a className="btn green" title="Want to Read" onClick={(e) => this.wantToRead(book)}>
-                                        <i className="fa fa-bookmark" aria-hidden="true"></i>
-                                    </a>
-                                </If>
-
-
-                                <If test={verifyCurrentlyReading == true}>
-                                    <a className="btn yellow" title="Reading" onClick={(e) => this.reading(book)}>
-                                        <i className="fa fa-bolt" aria-hidden="true" alt="Reading"></i>
-                                    </a>
-                                </If>
-
-                                <If test={verifyRead == true}>
-                                    <a className="btn blue" title="Read" onClick={(e) => this.read(book)}>
-                                        <i className="fa fa-check-square-o" aria-hidden="true" ></i>
-                                    </a>
-                                </If>
-
-                                <If test={verifyRemove == true}>
-                                    <a className="btn red" title="Remove from this list" onClick={(e) => this.onDeleteBook(book)}>
-                                        <i className="fa fa-times" aria-hidden="true" ></i>
-                                    </a>
-                                </If>
-
-                            </div>
-                        </div>
+                  <div className="book-title" id='title'>{title}</div>
+                    <div className="book-authors" id='author'>
+                        <ol>
+                            {book.authors.map((author, index) => (
+                                <span key={index}>
+                                    {author}
+                                </span>
+                            ))}
+                        </ol>
                     </div>
+
                 </div>
-
-                <SweetAlert
-                    showCancelButton={true}
-                    showConfirmButton={true}
-                    show={this.state.showConfirmRemove}
-                    title="Remove"
-                    confirmButtonText={"Yes"}
-                    cancelButtonText={"No"}
-                    onClose={() => this.setState({ showConfirmRemove: false })}
-                    text={'Would like to remove ' + book.title}
-                    onCancel={() => this.setState({ showConfirmRemove: false })}
-                    onConfirm={(e) => this.confirmDeleteBook(book)}
-                />
-
-                <SweetAlert
-                    show={this.state.showAddBook}
-                    title={"The book " + book.title + " was add."}
-                    confirmButtonColor="green"
-                    onConfirm={() => this.setState({ showAddBook: false })}
-                />
-
-                <SweetAlert
-                    show={this.state.showAdded}
-                    title={"The book " + book.title +" already was added" }
-                    confirmButtonColor="red"
-                    onConfirm={() => this.setState({ showAdded: false })}
-                />
-
-                <SweetAlert
-                    show={this.state.showError}
-                    title={"Oops! Something wrong happened!!" }
-                    confirmButtonColor="red"
-                    onConfirm={() => this.setState({ showError: false })}
-                />
-            </div>
-        )
-    }
+              </li>
+          </form>
+      </div>
+    )
+  }
 }
 
 export default BookCard
